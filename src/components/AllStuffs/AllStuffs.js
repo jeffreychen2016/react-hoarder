51bab759fa325ba5f8e463d973d8b5c834b9a297
Xuehui Chen <jeffreychen2016@gmail.com>
2018-07-12 22:37:54 -0500

    added allStuffs.js to get data from database for AllStuff component. Added Stuff component

--- src/components/AllStuffs/AllStuffs.js
+++ src/components/AllStuffs/AllStuffs.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './AllStuffs.css';
+import allStuffsRequests from '../../firebaseRequests/allStuffs';
+import Stuff from '../Stuff/Stuff';
 
 class AllStuffs extends React.Component {
+  state = {
+    allStuffs: [],
+  };
+
+  componentDidMount () {
+    allStuffsRequests
+      .getRequest()
+      .then((allStuffs) => {
+        this.setState({allStuffs});
+      })
+      .catch((err) => {
+        console.error('error with fish get request', err);
+      });
+  };
+
   render () {
+    const stuffComponent = this.state.allStuffs.map((stuff) => {
+      return (
+        <Stuff
+          details={stuff}
+        />
+      );
+    });
     return (
       <div className="AllStuffs">
         <h2>AllStuffs</h2>
+        <ul>
+          {stuffComponent}
+        </ul>
       </div>
     );
   }
---
+++ src/components/Stuff/Stuff.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Stuff extends React.Component {
+  render () {
+    return (
+      <div className="Stuff">
+        <h2>Stuff</h2>
+      </div>
+    );
+  }
+};
+
+export default Stuff;
